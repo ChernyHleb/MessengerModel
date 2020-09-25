f6321998abc5fceab6b0fa438a7a5811f07ea331
ChernyHleb <popovarlol@gmail.com>
2020-09-25 21:57:10 +0300

    Domain part is done

---
+++ Messenger/Domain/MessageModel/IMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Messenger.Domain.MessageModel
+{
+    public interface IMessage
+    {
+        public Guid Id { get; }
+        public Guid SenderUserId { get; }
+        public object Content { get; set; }
+        public DateTime CreationDateTime { get; }
+        public DateTime EditDateTime { get; }
+    }
+}
